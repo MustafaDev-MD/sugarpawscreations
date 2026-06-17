4a6bd656a27e40948294cbd2d262de13a0654d5d
MustafaDev-MD <mustafadeveloper87@gmail.com>
2026-06-18 03:38:05 +0500

    mobile responsive carousel width fix

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -41,19 +41,24 @@ class UserFactory extends Factory
     /**
      * Configure the model factory.
      */
+    // public function configure(): static
+    // {
+    //     return $this->afterCreating(function ($user) {
+    //         $team = Team::factory()->personal()->create([
+    //             'name' => $user->name . "'s Team",
+    //         ]);
+
+    //         $team->members()->attach($user, [
+    //             'role' => TeamRole::Owner->value,
+    //         ]);
+
+    //         $user->switchTeam($team);
+    //     });
+    // }
+
     public function configure(): static
     {
-        return $this->afterCreating(function ($user) {
-            $team = Team::factory()->personal()->create([
-                'name' => $user->name."'s Team",
-            ]);
-
-            $team->members()->attach($user, [
-                'role' => TeamRole::Owner->value,
-            ]);
-
-            $user->switchTeam($team);
-        });
+        return $this;
     }
 
     /**
@@ -61,7 +66,7 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
@@ -71,7 +76,7 @@ class UserFactory extends Factory
      */
     public function withTwoFactor(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'two_factor_secret' => encrypt('secret'),
             'two_factor_recovery_codes' => encrypt(json_encode(['recovery-code-1'])),
             'two_factor_confirmed_at' => now(),
