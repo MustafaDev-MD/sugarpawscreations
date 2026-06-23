64969dbd6a104fdc46cc784f7f74c23b7b55978a
MustafaDev-MD <mustafadeveloper87@gmail.com>
2026-06-24 04:41:28 +0500

    css update

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/', [HomeController::class, 'index'])
 Route::get('/category/{category:slug}', [CategoryController::class, 'show'])
     ->name('category.show');
 
-Route::get('/img/{path}', function ($path) {
+Route::get('/storage/img/{path}', function ($path) {
     $fullPath = public_path('uploads/' . $path);
     
     if (!file_exists($fullPath)) {
